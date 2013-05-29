12fed16f53b0a2c6334895241a49e5263e546cf6
Michael Klishin <michael@defprotocol.org>
2013-05-29 10:17:33 +0400

    Bump Langohr version

--- langohr/project.clj
+++ langohr/project.clj
@@ -1,8 +1,8 @@
 (defproject com.novemberain/acits "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure     "1.4.0"]
-                 [com.novemberain/langohr "1.0.0-beta11-SNAPSHOT"]
-                 [cheshire                "4.0.3"]]
+                 [com.novemberain/langohr "1.0.0-beta12"]
+                 [cheshire                "5.0.1"]]
   :source-paths      ["src/clojure"]
   :warn-on-reflection true
   :jvm-opts ["-Xmx512m"])
