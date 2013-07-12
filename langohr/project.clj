a897c65eac181bf08886542043cdd3df6d89602b
Michael Klishin <michael@defprotocol.org>
2013-07-13 02:10:44 +0400

    Langohr dependencies update

--- langohr/project.clj
+++ langohr/project.clj
@@ -1,8 +1,8 @@
 (defproject com.novemberain/acits "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure     "1.4.0"]
-                 [com.novemberain/langohr "1.0.0-beta12"]
-                 [cheshire                "5.0.1"]]
+  :dependencies [[org.clojure/clojure     "1.5.1"]
+                 [com.novemberain/langohr "1.0.0-beta14"]
+                 [cheshire                "5.2.0"]]
   :source-paths      ["src/clojure"]
   :warn-on-reflection true
-  :jvm-opts ["-Xmx512m"])
+  :jvm-opts ^:replace ["-Xmx512m"])
